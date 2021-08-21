f2652ebcbbff9481b84dc4a0df7846a0543b3011
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-08-21 14:44:45 -0400

    two ways

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,24 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  for (let i = 0; i < arr.length; i++){
+    for(let j = i + 1; j < arr.length; j++){
+      if (arr[i] === arr[i+1]){
+        return arr[i]
+      }
+    }
+  }
+return -1
+}
+
+function findFirstDuplicateObj(arr) {
+  let dictionary = {};
+
+for(let i = 0; i < arr.length; i++) {
+   if(dictionary[arr[i]] !== undefined)
+   return arr[i];
+else
+ dictionary[arr[i]] = i;
+}
+return -1;
 }
 
 if (require.main === module) {
@@ -11,9 +30,29 @@ if (require.main === module) {
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+
+  console.log("=>", findFirstDuplicate(([3, 4, 6, 6, 4, 6, 3])))
 }
 
 module.exports = findFirstDuplicate;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
